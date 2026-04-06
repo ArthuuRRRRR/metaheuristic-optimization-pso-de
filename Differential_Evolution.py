4e8553aa76683eaed1e877b7663ba438cb057269
ArthuuRRRRR <d.delhaye.arthur@gmail.com>
2026-04-06 22:05:40 +0200

    suite de

--- Differential_Evolution.py
+++ Differential_Evolution.py
@@ -2,12 +2,14 @@ import numpy as np
 from fitness_function import penaliser_algo
 
 class de:
-    def __init__(self, taille_population, nbr_dim, max_iter, min_iter, bornes):
+    def __init__(self, taille_population, nbr_dim, max_iter, min_iter, bornes, facteur_diff, taux_croisement):
         self.taille_population = taille_population
         self.nbr_dim = nbr_dim
         self.max_iter = max_iter
         self.min_iter = min_iter
         self.bornes = bornes
+        self.facteur_diff = facteur_diff
+        self.taux_croisement = taux_croisement
 
         self.population = np.array([[np.random.uniform(low, high) for (low, high) in bornes]for _ in range(taille_population)])
 
@@ -22,15 +24,30 @@ class de:
     
     def mutation(self, idx):
         indices = list(range(self.taille_population))
-        #indices.remove(idx)
-        
-        pass
+        indices.remove(idx)
 
-    def selection():
-        pass
+        a, b, c = np.random.choice(indices, 3, replace=False)
+
+        x_a = self.population[a]
+        x_b = self.population[b]
+        x_c = self.population[c]
+
+        vect_mutant = x_a + self.facteur_diff * (x_b - x_c)
+        vect_mutant_borne = self.gerer_bornes(vect_mutant)
+
+        return vect_mutant_borne
+
+    def selection(self, i):
+        result = penaliser_algo(self.population[i])
 
     def update_best():
         pass
 
-    def run(self) :
+    def croisement():
         pass
+
+    def run(self) :
+        compteur_sans_amelioration = 0
+        historique = []
+        for iteration in range(self.max_iter):
+            pass
